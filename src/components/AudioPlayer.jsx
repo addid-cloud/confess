631b4c804bb94bf781617239e67dc40bc4c88df5
evifrn <adit13062006@gmail.com>
2024-05-14 21:02:39 +0700

    fix audio

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -1,30 +1,35 @@
     import { Pause, Play } from '@phosphor-icons/react/dist/ssr';
-import React, { useState, useEffect } from 'react';
+    import React, { useState, useEffect } from 'react';
 
     const AudioPlayer = ({ src }) => {
-    const [audio] = useState(new Audio(src));
-    const [isPlaying, setIsPlaying] = useState(false);
+        const [audio, setAudio] = useState(null);
+        const [isPlaying, setIsPlaying] = useState(false);
 
-    const togglePlay = () => {
-    if (isPlaying) {
-        audio.pause();
-    } else {
-        audio.play();
-    }
-    setIsPlaying(!isPlaying);
-    };
+        useEffect(() => {
+        const audioElement = new Audio(src);
+        setAudio(audioElement);
 
-    useEffect(() => {
-    const handleEnded = () => {
-        setIsPlaying(false);
-    };
+        const handleEnded = () => {
+            setIsPlaying(false);
+        };
 
-    audio.addEventListener('ended', handleEnded);
+        audioElement.addEventListener('ended', handleEnded);
 
-    return () => {
-        audio.removeEventListener('ended', handleEnded);
-    };
-    }, [audio]);
+        return () => {
+            audioElement.removeEventListener('ended', handleEnded);
+        };
+        }, [src]);
+
+        const togglePlay = () => {
+        if (audio) {
+            if (isPlaying) {
+            audio.pause();
+            } else {
+            audio.play();
+            }
+            setIsPlaying(!isPlaying);
+        }
+        };
 
     return (
     <div>
